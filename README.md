82e68777d2002ad6c4fa6b0e90642aaa48a29a03
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2022-03-12 13:16:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# DenoScript
+
+Small framework for personal use for me to make scripts in Deno. Right now it's
+not much, but it's a work in progress. Meant to make scripting much easier and
+possibly even replace Bash (at least for me).
---
+++ mod.ts
@@ -0,0 +1,36 @@
+export const pwd = () => Deno.cwd();
+export const cd = (d: string) => Deno.chdir(d);
+export const exec = (f: string, ...a: string[]) => Deno.run({ cmd: [f, ...a] });
+
+export const ls = async (d?: string) => {
+  const dir = Deno.readDir(d || pwd());
+  const dirs: Deno.DirEntry[] = [];
+  for await (const member of dir) {
+    dirs.push(member);
+  }
+  return dirs;
+};
+
+export const cat = async (f: string, e = "utf-8") => {
+  const decoder = new TextDecoder(e);
+  const data = await Deno.readFile(f);
+  return decoder.decode(data);
+};
+
+export const isFile = async (f: string) => {
+  try {
+    await Deno.readFile(f);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
+export const isDir = async (d: string) => {
+  try {
+    await ls(d);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
